7252cb4da833d35b399093773523d74fcb404fd5
Homero Ochoa <homeroochoa47@gmail.com>
2023-01-06 23:33:36 -0800

    configuring logging for cloudwatch

--- core/settings.py
+++ core/settings.py
@@ -130,3 +130,19 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
